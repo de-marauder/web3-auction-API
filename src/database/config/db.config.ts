b568f5dfcf47632dd507cd587caeb0f40b35941f
de-marauder <ezikegodson@gmail.com>
2024-03-15 04:09:27 +0100

    added minimal config for mongoose schema ensuring that critical fields are hidden

---
+++ src/database/config/db.config.ts
@@ -0,0 +1,38 @@
+import { SchemaOptions } from '@nestjs/mongoose';
+
+export const dbSchemaOptions: SchemaOptions = {
+  id: true,
+  versionKey: false,
+  timestamps: true,
+  autoIndex: true,
+  toJSON: {
+    virtuals: true,
+    getters: true,
+    transform: (_, ret) => {
+      delete ret._id;
+      delete ret.password;
+      delete ret.salt;
+      delete ret.visible;
+      delete ret.secret;
+      delete ret.code;
+      delete ret.pin;
+      delete ret.accountActivated;
+      return ret;
+    },
+  },
+  toObject: {
+    virtuals: true,
+    getters: true,
+    transform: (_, ret) => {
+      delete ret._id;
+      delete ret.password;
+      delete ret.salt;
+      delete ret.visible;
+      delete ret.secret;
+      delete ret.code;
+      delete ret.pin;
+      delete ret.accountActivated;
+      return ret;
+    },
+  },
+};
