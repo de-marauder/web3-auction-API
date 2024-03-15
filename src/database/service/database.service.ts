c0f0ed1e7db0fddd701ab3f9fb51db5b7180b1a2
de-marauder <ezikegodson@gmail.com>
2024-03-15 04:13:31 +0100

    Database module complete with a base service that can be inherited by other services

---
+++ src/database/service/database.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
+@Injectable()
+export class DatabaseService {
+  private readonly logger = new Logger(DatabaseService.name);
+  constructor(
+    @InjectConnection()
+    private readonly connection: Connection,
+  ) { }
+
+  getDatabaseConnection(): Connection {
+    this.logger.log('database connection returned');
+    return this.connection;
+  }
+}
