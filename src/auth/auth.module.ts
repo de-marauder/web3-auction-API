8e722cff7e671521b9ffcc3e24492b6715e5db0a
de-marauder <ezikegodson@gmail.com>
2024-03-15 19:50:08 +0100

    update: added TokenService to AuthModule as a provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
 import { UserService } from 'src/user/service/user.service';
 import { UserModel } from 'src/user/model/user.model';
+import { TokenService } from 'src/token/service/token.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UserService],
+  providers: [AuthService, UserService, TokenService],
   imports: [MongooseModule.forFeatureAsync([UserModel])],
 })
 export class AuthModule {}
