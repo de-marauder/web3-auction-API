e49db89ea1d7ed656fbff29ec26f6ead45048413
de-marauder <ezikegodson@gmail.com>
2024-03-15 04:14:45 +0100

    Env Config and Database module hooked up to main application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { envValidator } from './env/validator/env.validator';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+      validationSchema: envValidator,
+    }),
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
