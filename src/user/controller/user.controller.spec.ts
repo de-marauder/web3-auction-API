7ccae3ab2f6d26aaaedd5911aea0a4b79fa0c7a2
de-marauder <ezikegodson@gmail.com>
2024-03-15 18:23:12 +0100

    added .env for test environment

--- src/user/controller/user.controller.spec.ts
+++ src/user/controller/user.controller.spec.ts
@@ -1,9 +1,12 @@
 import { Model } from 'mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getModelToken } from '@nestjs/mongoose';
 import { UserController } from './user.controller';
 import { UserService } from '../service/user.service';
 import { User } from '../schema/user.schema';
+import { TokenService } from 'src/token/service/token.service';
+import { envValidator } from 'src/env/validator/env.validator';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -13,10 +16,19 @@ describe('UserController', () => {
       controllers: [UserController],
       providers: [
         UserService,
+        TokenService,
+        ConfigService,
         {
           provide: getModelToken(User.name),
           useValue: Model<User>,
         },
+      ],
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: ['.env.test'],
+          validationSchema: envValidator,
+        }),
       ],
     }).compile();
 
