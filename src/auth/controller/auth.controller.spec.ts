0b6605f1a22992d9585a8702ec666e02059e9f69
de-marauder <ezikegodson@gmail.com>
2024-03-15 19:02:07 +0100

    added tests for login controller

--- src/auth/controller/auth.controller.spec.ts
+++ src/auth/controller/auth.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Model } from 'mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getModelToken } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { UserService } from 'src/user/service/user.service';
 import { User } from 'src/user/schema/user.schema';
-import { generateUniqueString } from 'src/utils/functions/utils.functions';
 import { BadRequestException } from '@nestjs/common';
+import { TokenService } from 'src/token/service/token.service';
+import { envValidator } from 'src/env/validator/env.validator';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -21,14 +23,14 @@ describe('AuthController', () => {
   user.email = testCreateUserDto.email;
   user.password = testCreateUserDto.password;
   user.avatar = testCreateUserDto.avatar;
-  user.username =
-    testCreateUserDto.username || `user-${generateUniqueString(4)}`;
+  user.username = testCreateUserDto.username || `user-xxxx`;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
       providers: [
         UserService,
+        TokenService,
         {
           provide: getModelToken(User.name),
           useValue: {
@@ -36,6 +38,13 @@ describe('AuthController', () => {
             findOne: jest.fn().mockResolvedValue(user),
           },
         },
+      ],
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: ['.env.test'],
+          validationSchema: envValidator,
+        }),
       ],
     }).compile();
     mockUserModel = module.get<Model<User>>(getModelToken(User.name));
@@ -69,6 +78,40 @@ describe('AuthController', () => {
       ];
       for (const test of tests) {
         expect(controller.register(test)).rejects.toThrow(BadRequestException);
+      }
+    });
+  });
+
+  describe('Login', () => {
+    it('Should return user object', async () => {
+      mockUserModel.findOne = jest.fn().mockResolvedValueOnce(null);
+      const { user: u } = await controller.login(testCreateUserDto);
+      expect(u).toMatchObject(user);
+    });
+
+    it('Should return user token', async () => {
+      mockUserModel.findOne = jest.fn().mockResolvedValueOnce(null);
+      const { token } = await controller.login(testCreateUserDto);
+      expect(token).toBeDefined();
+    });
+
+    it('Must accept password and email', async () => {
+      const tests = [
+        {
+          email: '',
+          password: '',
+        },
+        {
+          email: 'johndoe@gmail.com',
+          password: '',
+        },
+        {
+          email: '',
+          password: '<PASSWORD>',
+        },
+      ];
+      for (const test of tests) {
+        expect(controller.login(test)).rejects.toThrow(BadRequestException);
       }
     });
   });
