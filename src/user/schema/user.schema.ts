652b345bd94215b6be92dccd453c75c12c604e88
de-marauder <ezikegodson@gmail.com>
2024-03-15 04:53:41 +0100

    defined user schema

---
+++ src/user/schema/user.schema.ts
@@ -0,0 +1,28 @@
+import * as mongoose from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { dbSchemaOptions } from 'src/database/config/db.config';
+
+@Schema(dbSchemaOptions)
+export class User {
+  @Prop({ type: String, default: '' })
+  avatar: string;
+
+  @Prop({ type: String })
+  username: string;
+
+  @Prop({ type: String, index: true })
+  email: string;
+
+  @Prop({ type: String, required: true, select: false })
+  password: string;
+
+  @Prop({ type: Boolean, default: false })
+  activated: boolean;
+
+  @Prop({ type: Boolean, default: false, select: false })
+  deleted: boolean;
+}
+
+export type UserDocument = User & mongoose.Document;
+
+export const UserSchema = SchemaFactory.createForClass(User);
