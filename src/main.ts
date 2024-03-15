5465739070c78225c1e72cdf631c9fde1e494517
de-marauder <ezikegodson@gmail.com>
2024-03-16 00:59:38 +0100

    added exception handler globally and set SPI prefix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { envConfigValidator } from './config/config.validator';
 import { EnvConfigEnum } from './config/env.enum';
+import { GlobalExceptionsFilter } from './utils/exception/global.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new GlobalExceptionsFilter(httpAdapter));
 
   app.enableCors();
+  /**To support payload with large size */
+  app.use(express.json({ limit: '100mb' }));
+  app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
   await envConfigValidator.validateAsync(process.env, {
     stripUnknown: true,
@@ -17,6 +24,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const port = configService.get<number>(EnvConfigEnum.PORT);
+  app.setGlobalPrefix('api/v1');
 
   await app.listen(port || 3000);
   Logger.debug(`listening on port ${port}`);
