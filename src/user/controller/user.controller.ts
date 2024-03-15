9528e8df1f007d7a82237cf634e56c9603590287
de-marauder <ezikegodson@gmail.com>
2024-03-15 18:21:37 +0100

    added controller for verifying users and activating their accounts

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,9 +1,45 @@
-import { Controller, Logger } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Logger,
+  Post,
+} from '@nestjs/common';
+
 import { UserService } from '../service/user.service';
+import { VerifyUserDto } from '../dto/user.dto';
+import { TokenService } from 'src/token/service/token.service';
 
 @Controller('user')
 export class UserController {
   private readonly logger = new Logger(UserController.name);
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private tokenService: TokenService,
+  ) { }
+
+  @Post('verify')
+  async verifyUser(
+    @Body()
+    { email, code }: VerifyUserDto,
+  ) {
+    const user = await this.userService.findOneOrErrorOut({ email });
+
+    if (user.verificationCode !== code) {
+      throw new BadRequestException();
+    }
+
+    user.activated = true;
+    user.verificationCode = '';
+
+    const token = await this.tokenService.signToken({
+      email: user.email,
+      username: user.username,
+      id: user.id,
+    });
+
+    await user.save();
+    return { user, token };
+  }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserController } from './controller/user.controller';
 import { UserService } from './service/user.service';
 import { UserModel } from './model/user.model';
+import { TokenService } from 'src/token/service/token.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, TokenService],
   imports: [MongooseModule.forFeatureAsync([UserModel])],
   exports: [MongooseModule.forFeatureAsync([UserModel])],
 })
-export class UserModule {}
+export class UserModule { }
