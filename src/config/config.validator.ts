794c1b3d9e50a499542f8075cecff13f2c627c23
de-marauder <ezikegodson@gmail.com>
2024-03-17 12:13:06 +0100

    added validation for web3 provider url

--- src/config/config.validator.ts
+++ src/config/config.validator.ts
@@ -1,11 +1,17 @@
 import * as Joi from 'joi';
 import { EnvConfigEnum } from './env.enum';
 import { envValidator } from 'src/env/validator/env.validator';
+import {
+  numberValidator,
+  stringValidator,
+  urlValidator,
+} from 'src/utils/validator/custom.validator';
 
 export const envConfigValidator = envValidator.append({
-  [EnvConfigEnum.PORT]: Joi.number().required(),
-  [EnvConfigEnum.TOKEN_SECRET]: Joi.string().required(),
-  [EnvConfigEnum.DEV_DATABASE]: Joi.string().required(),
-  [EnvConfigEnum.TEST_DATABASE]: Joi.string().required(),
-  [EnvConfigEnum.JWT_EXPIRE]: Joi.number(),
+  [EnvConfigEnum.PORT]: numberValidator.required(),
+  [EnvConfigEnum.TOKEN_SECRET]: stringValidator.required(),
+  [EnvConfigEnum.DEV_DATABASE]: stringValidator.required(),
+  [EnvConfigEnum.TEST_DATABASE]: stringValidator.required(),
+  [EnvConfigEnum.JWT_EXPIRE]: numberValidator.required(),
+  [EnvConfigEnum.WEB3_PROVIDER_URL]: urlValidator.required(),
 });
