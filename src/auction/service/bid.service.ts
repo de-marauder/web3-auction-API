302658a14ef2aec6f3796fe699b7841d28babeba
de-marauder <ezikegodson@gmail.com>
2024-03-17 17:30:52 +0100

    added conveinience function for parsing bid duration

--- src/auction/service/bid.service.ts
+++ src/auction/service/bid.service.ts
@@ -1,8 +1,9 @@
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { BaseService } from 'src/database/service/db.service';
 import { Bid } from '../schema/bid.schema';
+import { TimeUnits } from 'src/utils/enums/utils.enums';
 
 @Injectable()
 export class BidService extends BaseService<Bid> {
@@ -17,4 +18,54 @@ export class BidService extends BaseService<Bid> {
   async getHighestBid() { }
 
   async getStatistics() { }
+
+  parsebiddingTime(time: string) {
+    const lastChar = time[time.length - 1];
+    this.logger.log('typeof time: ', typeof time);
+    this.logger.log('lastChar: ', lastChar);
+    const validLastChars = ['s', 'm', 'h', 'd', 'w', 'M', 'Y'];
+    if (isNaN(+lastChar) && !validLastChars.includes(lastChar)) {
+      throw new BadRequestException(
+        'Invalid bid time. Only s, m, h, d, w, M, Y are vaild time units',
+      );
+    }
+    const timeWithoutLastChar = +time.slice(0, time.length - 1);
+    this.logger.log('timeWithoutLastChar: ', timeWithoutLastChar);
+    this.logger.log('typeof timeWithoutLastChar: ', typeof timeWithoutLastChar);
+    if (isNaN(timeWithoutLastChar)) {
+      throw new BadRequestException(
+        'Invalid bid time. Values before a unit (if passed) should be a valid number'
+      );
+    }
+    let biddingTime: number;
+    const now = Date.now();
+    switch (lastChar) {
+      case 's':
+        biddingTime = now + timeWithoutLastChar * TimeUnits.SECOND;
+        break;
+      case 'm':
+        biddingTime = now + timeWithoutLastChar * TimeUnits.MINUTE;
+        break;
+      case 'h':
+        biddingTime = now + timeWithoutLastChar * TimeUnits.HOUR;
+        break;
+      case 'd':
+        biddingTime = now + timeWithoutLastChar * TimeUnits.DAY;
+        break;
+      case 'w':
+        biddingTime = now + timeWithoutLastChar * TimeUnits.WEEK;
+        break;
+      case 'M':
+        biddingTime = now + timeWithoutLastChar * TimeUnits.SECOND;
+        break;
+      case 'Y':
+        biddingTime = now + timeWithoutLastChar * TimeUnits.SECOND;
+        break;
+      default:
+        if (isNaN(+time)) throw new BadRequestException('Invalid bid time');
+        biddingTime = now + +time; // default to milliseconds
+        break;
+    }
+    return biddingTime;
+  }
 }
--- src/utils/enums/utils.enums.ts
+++ src/utils/enums/utils.enums.ts
@@ -5,3 +5,13 @@ export enum ResponseStatusEnum {
   SUCCESS = 'SUCCESS',
   FAIL = 'FAIL',
 }
+export enum TimeUnits {
+  MILLISECONDS = 1,
+  SECOND = 1000 * TimeUnits.MILLISECONDS,
+  MINUTE = 60 * TimeUnits.SECOND,
+  HOUR = 60 * TimeUnits.MINUTE,
+  DAY = 24 * TimeUnits.HOUR,
+  WEEK = 7 * TimeUnits.DAY,
+  MONTH = 30 * TimeUnits.DAY,
+  YEAR = 365 * TimeUnits.DAY,
+}
