d40230f171ad3f2b92ac8f6a167a8c354d550f52
de-marauder <ezikegodson@gmail.com>
2024-03-17 17:18:00 +0100

    update: removed unnecessary import by requesting only the default module export

--- src/web3/service/web3.service.ts
+++ src/web3/service/web3.service.ts
@@ -1,18 +1,18 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { EnvConfigEnum } from 'src/config/env.enum';
-import * as Web3 from 'web3';
+import { default as Web3 } from 'web3';
 
 @Injectable()
 export class Web3Service {
   private logger = new Logger(Web3Service.name);
 
-  web3: Web3.Web3;
+  web3: Web3;
   constructor(private config: ConfigService) {
     // get provider URL from ENV configuration
     const providerUrl = config.getOrThrow(EnvConfigEnum.WEB3_PROVIDER_URL);
-    const provider = new Web3.providers.http.HttpProvider(providerUrl);
+    const provider = new Web3.providers.HttpProvider(providerUrl);
     // initialize and expose web3 object
-    this.web3 = new Web3.Web3(provider);
+    this.web3 = new Web3(provider);
   }
 }
