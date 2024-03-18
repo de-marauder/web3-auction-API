e5a95aca327ef382a6841141b9e7ae5468d60fb5
de-marauder <ezikegodson@gmail.com>
2024-03-18 08:14:54 +0100

    update: added custom errors for auction bids

--- src/utils/enums/utils.enums.ts
+++ src/utils/enums/utils.enums.ts
@@ -1,13 +1,16 @@
 export enum Errors {
   TOKEN_EXPIRED = 'TOKEN_EXPIRED',
+  AUCTION_HAS_HIGHER_BID = 'There already is a higher bid',
+  AUCTION_HAS_ENDED = 'Auction already ended',
+  AUCTION_BID_VALUE_INVALID = 'Auction bid value invalid. Please pass a valid bid in ether',
 }
 export enum ResponseStatusEnum {
   SUCCESS = 'SUCCESS',
   FAIL = 'FAIL',
 }
 export enum TimeUnits {
-  MILLISECONDS = 1,
-  SECOND = 1000 * TimeUnits.MILLISECONDS,
+  SECOND = 1,
+  MILLISECONDS = TimeUnits.SECOND / 1000,
   MINUTE = 60 * TimeUnits.SECOND,
   HOUR = 60 * TimeUnits.MINUTE,
   DAY = 24 * TimeUnits.HOUR,
