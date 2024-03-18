73d2f3278325b96c26b7f4a7f052b6f387760697
de-marauder <ezikegodson@gmail.com>
2024-03-18 13:12:28 +0100

    update: improved validation for bid value

--- src/auction/validator/auction.validator.ts
+++ src/auction/validator/auction.validator.ts
@@ -10,5 +10,14 @@ export const DeployAuctionDtoValidator = Joi.object<deployAuctionDto>({
 
 export const SubmitBidDtoValidator = Joi.object<SubmitBidDto>({
   from: stringValidator.required(),
-  value: stringValidator.required(),
+  value: stringValidator
+    .required()
+    .custom((value: string, helpers: Joi.CustomHelpers) => {
+      console.log('Validating value: ', value);
+      if (isNaN(+value)) {
+      }
+      const error = helpers.error('InvalidBidValue');
+      error.message = `${error.code}: Pass a number as a string`;
+      return error;
+    }),
 });
