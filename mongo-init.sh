b683ab9d22f056548c35c5d17cac35105a2a5238
de-marauder <ezikegodson@gmail.com>
2024-03-22 17:43:10 +0100

    update: improved support for dev, test and prod environments

--- mongo-init.sh
+++ mongo-init.sh
@@ -3,18 +3,39 @@
 
 set -e
 
-mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD <<EOF
-print('Configuring $API_DB_USER...');
+NEW_DB=""
+NEW_DB_USER=""
+NEW_DB_USER_PASSWORD=""
+
+if [[ $NODE_ENV == "development" ]]; then
+  NEW_DB="$API_DB"
+  NEW_DB_USER="$API_DB_USER"
+  NEW_DB_USER_PASSWORD="$API_DB_PASSWORD"
+
+elif [[ $NODE_ENV == "production" ]]; then
+  NEW_DB="$API_PROD_DB"
+  NEW_DB_USER="$API_PROD_DB_USER"
+  NEW_DB_USER_PASSWORD="$API_PROD_DB_PASSWORD"
+
+elif [[ $NODE_ENV == "test" ]]; then
+  NEW_DB="$API_TEST_DB"
+  NEW_DB_USER="$API_TEST_DB_USER"
+  NEW_DB_USER_PASSWORD="$API_TEST_DB_PASSWORD"
+fi
+
+# Connect to MongoDB and configure the database
+mongo -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" <<EOF
+print('Configuring $NEW_DB_USER...');
 print('');
 
-print("Creating $API_DB...");
+print("Creating '$NEW_DB'...");
 print('');
-db = db.getSiblingDB('$API_DB');
+db = db.getSiblingDB('$NEW_DB');
 
 // Check if the user already exists
-print("Checking if user $API_DB_USER already exists")
+print("Checking if user '$NEW_DB_USER' already exists");
 print('');
-userExists = db.system.users.findOne({ user: '$API_DB_USER' });
+userExists = db.system.users.findOne({ user: '$NEW_DB_USER' });
 print('');
 if (!userExists) {
   // Create the user with read-write access to the 'api' database
@@ -22,15 +43,15 @@ if (!userExists) {
   print('');
   
   db.createUser({
-    user: '$API_DB_USER',
-    pwd: '$API_DB_PASSWORD',
+    user: '$NEW_DB_USER',
+    pwd: '$NEW_DB_USER_PASSWORD',
     roles: [
-      { role: 'readWrite', db: '$API_DB' }
+      { role: 'readWrite', db: '$NEW_DB' }
     ]
   });
 
-  print('Successfully created user $API_DB_USER with read-write access to the api database.');
+  print("Successfully created user '$NEW_DB_USER' with read-write access to the api database.");
 } else {
-  print('User $API_DB_USER already exists. Skipping creation.');
+  print("User '$NEW_DB_USER' already exists. Skipping creation.");
 }
 EOF
